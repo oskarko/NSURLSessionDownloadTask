db00cd0b9ee5382d3e8cb2bc79c16832381eb3c5
Oscar <orodriguez@whatscine.es>
2017-05-23 15:23:11 +0200

    Initial Commit

---
+++ DownloadTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DownloadTask
+//
+//  Created by Oscar Rodriguez Garrucho on 23/5/17.
+//  Copyright © 2017 Oscar Rodriguez Garrucho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DownloadTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DownloadTask
+//
+//  Created by Oscar Rodriguez Garrucho on 23/5/17.
+//  Copyright © 2017 Oscar Rodriguez Garrucho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
